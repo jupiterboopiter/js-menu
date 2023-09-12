a9034d9d83981e591b125affe6b9ca75b325da36
Elisa Elezi <elisaelezi12@gmail.com>
2023-09-12 11:57:27 -0400

    Moved to function over to it's own so that it's easier to filter

--- script.js
+++ script.js
@@ -84,7 +84,11 @@ const menu = [
 const sectionCenter = document.querySelector('.section-center');
 
 window.addEventListener('DOMContentLoaded', function() {
-    let displayMenu = menu.map(function(item) {
+    displayMenuItems(menu);
+});
+
+function displayMenuItems(menuItems) {
+    let displayMenu = menuItems.map(function(item) {
 
         return `<article class="menu-item">
         <img src=${item.img} class="photo" alt=${item.title}>
@@ -102,4 +106,4 @@ window.addEventListener('DOMContentLoaded', function() {
     });
     displayMenu = displayMenu.join("");
     sectionCenter.innerHTML = displayMenu;
-});
+}
