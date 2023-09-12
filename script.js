946fad395240734ce389ab2b99ca40e4d782c2e6
Elisa Elezi <elisaelezi12@gmail.com>
2023-09-12 12:30:53 -0400

    Categories are now functional

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// items
 const menu = [
     {
         id: 1,
@@ -82,10 +83,37 @@ const menu = [
 ];
 
 const sectionCenter = document.querySelector('.section-center');
+const filterBtns = document.querySelectorAll('.filter-btn');
 
+
+// load items
 window.addEventListener('DOMContentLoaded', function() {
     displayMenuItems(menu);
 });
+// filter items
+filterBtns.forEach(function(btn) {
+    btn.addEventListener('click', function(e) {
+        const category = e.currentTarget.dataset.id;
+        const menuCategory = menu.filter(function(menuItem) {
+            if (menuItem.category === category) {
+                return menuItem;
+            }
+        });
+        if(category === 'all') {
+            displayMenuItems(menu);
+        } else {
+            displayMenuItems(menuCategory);
+        }
+    });
+});
+
+
+
+
+
+
+
+
 
 function displayMenuItems(menuItems) {
     let displayMenu = menuItems.map(function(item) {
@@ -107,3 +135,4 @@ function displayMenuItems(menuItems) {
     displayMenu = displayMenu.join("");
     sectionCenter.innerHTML = displayMenu;
 }
+
